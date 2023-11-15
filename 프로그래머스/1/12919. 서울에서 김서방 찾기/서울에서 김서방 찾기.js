402d08bc3847629262cdbf553c5814f46303fd47
SENG <149942572+kozneokhan@users.noreply.github.com>
2023-11-15 20:46:29 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -2,3 +2,6 @@ function solution(seoul) {
     let i = seoul.indexOf("Kim");
     return `김서방은 ${i}에 있다`;
 }
+
+// findIndex 방법
+//const solution = (arr) => `김서방은 ${arr.findIndex(s => s === 'Kim')}에 있다`;
