894e4bf7abf619cd1335599807833172a909a168
SENG <149942572+kozneokhan@users.noreply.github.com>
2023-11-17 09:24:45 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -8,3 +8,17 @@ function solution(n) {
 }
 
 // n을 x로 나눈 값이 아닌 나머지 남는 숫자가 1이 되도록 하는 가장 작은 자연수 x를 리턴 
+
+// 천재분의 while 풀이법 
+// function solution(n, x = 1) {    
+//     while (x++) {
+//         if (n % x === 1) {
+//             return x;
+//         }
+//     }    
+// }
+
+// 천재분의 3항연산자 풀이법
+// function solution(n, x = 0) {
+//     return n % x === 1 ? x : solution(n, x + 1);
+// }
