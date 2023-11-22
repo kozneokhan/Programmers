aa2eef25fbe6efb2452a49e4de56b80ed58c3160
SENG <149942572+kozneokhan@users.noreply.github.com>
2023-11-22 23:19:57 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -1,12 +1,23 @@
-function solution(price, money, count) {
- let totalPrice = 0;
-    for(let i = 0; i<=count; i++){
-        totalPrice += price * i
-    }
-    return money - totalPrice > 0 ? 0: totalPrice - money
-}
+// function solution(price, money, count) {
+//  let totalPrice = 0;
+//     for(let i = 0; i<=count; i++){
+//         totalPrice += price * i
+//     }
+//     return money - totalPrice > 0 ? 0: totalPrice - money
+// }
 
 // 가격 3 
 // 돈 20 
 // 탄 횟수 4
 // 남은 결과값 10
+
+function solution(price, money, count) {
+    // 등차수열의 합 공식을 활용하여 필요한 총 이용 금액 계산
+    const totalCost = (count * (count + 1) / 2) * price;
+
+    // 부족한 금액 계산
+    const requiredMoney = totalCost - money;
+
+    // 필요한 금액 반환 (부족한 경우에는 음수가 아닌 부족한 금액 반환)
+    return requiredMoney > 0 ? requiredMoney : 0;
+}
