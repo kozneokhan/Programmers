42717b4c8f8f4bdfe8e8f8fe43a1742d09e64996
SENG <149942572+kozneokhan@users.noreply.github.com>
2023-11-24 21:49:23 +0900

    [level 1] Title: 하샤드 수, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -1,12 +1,17 @@
+// function solution(x) {
+//     const num = x.toString();
+//     let sum = 0;
+//     for (let i = 0; i <num.length; i++) {
+//         sum += parseInt(num[i]);
+//     }
+//     if (x % sum === 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+
 function solution(x) {
-    const num = x.toString();
-    let sum = 0;
-    for (let i = 0; i <num.length; i++) {
-        sum += parseInt(num[i]);
-    }
-    if (x % sum === 0) {
-        return true;
-    } else {
-        return false;
-    }
+    return x%eval([...x.toString()].join("+")) ? false : true;
 }
