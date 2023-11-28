942a3d0a9cfe2b8a355de78c0bf57ad101d14f7b
SENG <149942572+kozneokhan@users.noreply.github.com>
2023-11-28 09:39:04 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,19 +1,29 @@
+// function solution(num) {
+//     let answer = 0;  // 작업 횟수를 기록할 변수
+
+//     while (num !== 1) {
+//         if (answer >= 500) {  // 500번 이상 반복하여도 1이 되지 않는 경우
+//             return -1;
+//         }
+
+//         if (num % 2 === 0) {  // 주어진 수가 짝수인 경우
+//             num /= 2;
+//         } else {  // 주어진 수가 홀수인 경우
+//             num = num * 3 + 1;
+//         }
+
+//         answer++;  // 작업 횟수 증가
+//     }
+
+//     return answer;  // 작업을 반복한 횟수 반환
+// }
+
+
 function solution(num) {
-    let answer = 0;  // 작업 횟수를 기록할 변수
-
-    while (num !== 1) {
-        if (answer >= 500) {  // 500번 이상 반복하여도 1이 되지 않는 경우
-            return -1;
-        }
-
-        if (num % 2 === 0) {  // 주어진 수가 짝수인 경우
-            num /= 2;
-        } else {  // 주어진 수가 홀수인 경우
-            num = num * 3 + 1;
-        }
-
-        answer++;  // 작업 횟수 증가
-    }
-
-    return answer;  // 작업을 반복한 횟수 반환
+    var answer = 0;
+    while(num !=1 && answer !=500){
+        num%2==0 ? num = num/2 : num = num*3 +1;
+    answer++;
+  }
+    return num == 1 ? answer : -1;
 }
