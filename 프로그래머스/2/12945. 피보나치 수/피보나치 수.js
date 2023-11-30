d9eb92712dcbf155e4ce80992c9929054bff8aa8
SENG <149942572+kozneokhan@users.noreply.github.com>
2023-11-30 14:56:39 +0900

    [level 2] Title: 피보나치 수, Time: 3.88 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -6,3 +6,11 @@ function solution(n) {
     }
     return answer[n] % 1234567
 }
+
+function solution(n) {
+    let [a, b] = [0, 1]; // 초기값 설정: a = 0, b = 1
+    while (--n) {        // n이 0이 될 때까지 반복
+        [a, b] = [b, (a + b) % 1234567]; // 배열 비구조화 할당을 사용하여 a와 b를 갱신
+    }
+    return b;            // 최종적으로 n번째 피보나치 수를 1234567로 나눈 나머지 반환
+}
