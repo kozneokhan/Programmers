9ba3f8442f2a07fe381128eac64ebaa97e89e098
SENG <149942572+kozneokhan@users.noreply.github.com>
2023-12-01 10:21:59 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,8 +1,13 @@
+// function solution(numbers) {
+//     var answer = 0;
+//     for(let i= 0; i <= 9; i++)
+//         if(!numbers.includes(i)){
+//            answer += i;
+//            } 
+//     return answer;
+// }
+
+
 function solution(numbers) {
-    var answer = 0;
-    for(let i= 0; i <= 9; i++)
-        if(!numbers.includes(i)){
-           answer += i;
-           } 
-    return answer;
+    return 45 - numbers.reduce((cur, acc) => cur + acc, 0);
 }
