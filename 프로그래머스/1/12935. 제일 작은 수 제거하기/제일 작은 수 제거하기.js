68ccdb50b53ab3ccc54c95757884f6430cfa8dd7
SENG <149942572+kozneokhan@users.noreply.github.com>
2023-12-06 01:26:37 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -0,0 +1,23 @@
+function solution(arr) {
+    if (arr.length <= 1) {
+        return [-1];
+    }
+
+    // 배열에서 최솟값 찾기
+    let min = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    // 최솟값을 제외한 새로운 배열 생성
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== min) {
+            result.push(arr[i]);
+        }
+    }
+
+    return result.length > 0 ? result : [-1];
+}
