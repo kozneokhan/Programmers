6ed629728e93dc7a98f6d865f9f222a912f73839
SENG <149942572+kozneokhan@users.noreply.github.com>
2023-12-06 01:41:20 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,26 +1,26 @@
-function solution(arr) {
-    if (arr.length <= 1) {
-        return [-1];
-    }
+// function solution(arr) {
+//     if (arr.length <= 1) {
+//         return [-1];
+//     }
 
-    // 배열에서 최솟값 찾기
-    let min = arr[0];
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] < min) {
-            min = arr[i];
-        }
-    }
+//     // 배열에서 최솟값 찾기
+//     let min = arr[0];
+//     for (let i = 1; i < arr.length; i++) {
+//         if (arr[i] < min) {
+//             min = arr[i];
+//         }
+//     }
 
-    // 최솟값을 제외한 새로운 배열 생성
-    const result = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] !== min) {
-            result.push(arr[i]);
-        }
-    }
+//     // 최솟값을 제외한 새로운 배열 생성
+//     const result = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] !== min) {
+//             result.push(arr[i]);
+//         }
+//     }
 
-    return result.length > 0 ? result : [-1];
-}
+//     return result.length > 0 ? result : [-1];
+// }
 
 // function solution(arr) {
 //     if (arr.length <= 1) {
@@ -35,3 +35,8 @@ function solution(arr) {
 
 //     return result.length > 0 ? result : [-1];
 // }
+
+function solution(arr) {
+    const min = Math.min(...arr);
+    return arr.length !== 1 ? arr.filter(i => i !== min) : [-1]
+}
