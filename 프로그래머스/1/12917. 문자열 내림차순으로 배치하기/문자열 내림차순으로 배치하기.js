7c45f5afac6ad072100fd0a7f63642a69a9f2a3d
SENG <149942572+kozneokhan@users.noreply.github.com>
2023-12-11 10:31:11 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,3 +1,18 @@
 function solution(s) {
    return String(s).split("").sort().reverse().join("");
 }
+
+// function solution(s) {
+//     let answer = [[],[]];
+//     for (let i=0; i<s.length; i++) {
+//         if(s[i] === s[i].toUpperCase()) {
+//             answer[1].push(s[i])
+//         } else {
+//             answer[0].push(s[i])
+//         }
+//     }
+//     let ans1 = answer[0].sort().reverse().join('');
+//     let ans2 = answer[1].sort().reverse().join('');
+//     answer = [ans1, ans2].join('')
+//     return answer
+// }
