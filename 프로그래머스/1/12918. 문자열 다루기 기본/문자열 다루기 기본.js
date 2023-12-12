2a95b022157849537f396a877548d9cd529e9307
SENG <149942572+kozneokhan@users.noreply.github.com>
2023-12-12 09:55:48 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,12 +1,16 @@
+ // function solution(s) {
+ //     if (s.length === 4 || s.length === 6) {
+ //         for (let i = 0; i < s.length; i++) {
+ //             if (isNaN(s[i])) {
+ //                return false;
+ //             }
+ //         }
+ //         return true;
+ //     } else {
+ //         return false;
+ //     }
+ // }
+
 function solution(s) {
-    if (s.length === 4 || s.length === 6) {
-        for (let i = 0; i < s.length; i++) {
-            if (isNaN(s[i])) {
-                return false;
-            }
-        }
-        return true;
-    } else {
-        return false;
-    }
+    return (s.length === 4 || s.length === 6) && s == parseInt(s)
 }
