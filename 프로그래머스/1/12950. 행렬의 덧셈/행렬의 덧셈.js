2ae51651e4b8d31b58916f2c94fe299fa158a031
SENG <149942572+kozneokhan@users.noreply.github.com>
2023-12-13 09:42:14 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.05 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,19 +1,22 @@
-function solution(arr1, arr2) {
-    // 결과를 저장할 빈 행렬 생성
-    let answer = [];
+// function solution(arr1, arr2) {
+//     // 결과를 저장할 빈 행렬 생성
+//     let answer = [];
     
-    // 행렬의 행 수와 열 수 구하기
-    let rows = arr1.length;
-    let cols = arr1[0].length;
+//     // 행렬의 행 수와 열 수 구하기
+//     let rows = arr1.length;
+//     let cols = arr1[0].length;
     
-    // 각 위치의 원소를 더해서 새로운 행렬에 추가
-    for (let i = 0; i < rows; i++) {
-        let row = [];
-        for (let j = 0; j < cols; j++) {
-            row.push(arr1[i][j] + arr2[i][j]);
-        }
-        answer.push(row);
-    }
+//     // 각 위치의 원소를 더해서 새로운 행렬에 추가
+//     for (let i = 0; i < rows; i++) {
+//         let row = [];
+//         for (let j = 0; j < cols; j++) {
+//             row.push(arr1[i][j] + arr2[i][j]);
+//         }
+//         answer.push(row);
+//     }
     
-    return answer;
+//     return answer;
+// }
+function solution(A,B){
+    return A.map((arr1, idx1) => arr1.map((val, idx2) => val+B[idx1][idx2]));
 }
