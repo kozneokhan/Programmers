0f7b85a84a3eeb1d30acafb0c9533b37e3afe263
SENG <149942572+kozneokhan@users.noreply.github.com>
2023-12-17 14:56:13 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,26 +1,32 @@
-function solution(n, m) {
-    // 최대공약수 계산
-    function gcd(a, b) {
-        for (let i = Math.min(a, b); i > 0; i--) {
-            if (a % i === 0 && b % i === 0) {
-                return i;
-            }
-        }
-        return 1; // 1은 모든 수의 공약수이므로 최소값으로 설정
-    }
+// function solution(n, m) {
+//     // 최대공약수 계산
+//     function gcd(a, b) {
+//         for (let i = Math.min(a, b); i > 0; i--) {
+//             if (a % i === 0 && b % i === 0) {
+//                 return i;
+//             }
+//         }
+//         return 1; // 1은 모든 수의 공약수이므로 최소값으로 설정
+//     }
 
-    // 최소공배수 계산
-    function lcm(a, b) {
-        for (let i = Math.max(a, b); ; i += Math.max(a, b)) {
-            if (i % a === 0 && i % b === 0) {
-                return i;
-            }
-        }
-    }
+//     // 최소공배수 계산
+//     function lcm(a, b) {
+//         for (let i = Math.max(a, b); ; i += Math.max(a, b)) {
+//             if (i % a === 0 && i % b === 0) {
+//                 return i;
+//             }
+//         }
+//     }
 
-    // 결과 배열에 최대공약수와 최소공배수 추가
-    const gcdResult = gcd(n, m);
-    const lcmResult = lcm(n, m);
+//     // 결과 배열에 최대공약수와 최소공배수 추가
+//     const gcdResult = gcd(n, m);
+//     const lcmResult = lcm(n, m);
 
-    return [gcdResult, lcmResult];
+//     return [gcdResult, lcmResult];
+// }
+
+function solution(a, b) {
+    var r;
+    for(var ab= a*b;r = a % b;a = b, b = r){}
+    return [b, ab/b];
 }
